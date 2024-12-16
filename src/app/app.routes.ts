cda7adf8e8504e6cc13d8fff376ce47b7d268fa2
Gabriel Nabeta <gabrielnabeta@gmail.com>
2024-12-16 18:14:14 -0300

    Criando componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'menu', title: 'Menu', component:MenuComponent},
+    {path: 'home', title: 'Home', component:HomeComponent},
+    {path: 'footer', title: 'Footer', component:FooterComponent}
+];
